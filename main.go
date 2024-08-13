48b33c64c1fb84cb0256ec36ca02396f354fa1b4
Aurora <programming.aurora@gmail.com>
2024-08-13 22:17:25 +0100

    basic window

--- main.go
+++ main.go
@@ -20,12 +20,32 @@ func main() {
 	np1 := ui.NPatchBox{Texture: rl.LoadTexture("sprites/Panel.png"), NPatchInfo: rl.NPatchInfo{Left: 8, Right: 8, Top: 8, Bottom: 8, Source: rl.Rectangle{Width: 128, Height: 128}}}
 
 	np1.Layout(np1Size)
+
+	w1 := ui.Window{
+		Area: ui.Area{
+			X:      600,
+			Y:      300,
+			Width:  480,
+			Height: 360,
+		},
+		Content: &ui.PieChart{
+			Segments: []ui.ChartSegment{
+				{Col: rl.DarkGreen, N: 10},
+				{Col: rl.DarkPurple, N: 6},
+				{Col: rl.DarkBrown, N: 4},
+				{Col: rl.DarkGray, N: 1},
+			},
+		},
+	}
 	for !rl.WindowShouldClose() {
+		w1.Update()
 		rl.BeginDrawing()
 
 		rl.ClearBackground(rl.RayWhite)
 
 		np1.Draw(&uiCtx)
+
+		w1.Draw()
 
 		rl.DrawText("Wunkopolis", 285, 200, 40, rl.Black)
 		rl.EndDrawing()
---
+++ ui/window.go
@@ -0,0 +1,56 @@
+package ui
+
+import rl "github.com/gen2brain/raylib-go/raylib"
+
+const topBarSize = 32
+const borderSize = 2
+
+type Window struct {
+	Content  UIElem
+	Title    string
+	Area     Area
+	dragging bool
+}
+
+func (w *Window) barArea() Area {
+	return Area{
+		X:      w.Area.X,
+		Y:      w.Area.Y,
+		Width:  w.Area.Width,
+		Height: topBarSize,
+	}
+}
+
+func (w *Window) Update() {
+	barArea := w.barArea()
+	if rl.IsMouseButtonPressed(rl.MouseButtonLeft) && rl.CheckCollisionPointRec(rl.GetMousePosition(), barArea) {
+		w.dragging = true
+		return
+	}
+
+	if w.dragging && rl.IsMouseButtonReleased(rl.MouseButtonLeft) {
+		w.dragging = false
+		return
+	}
+
+	if w.dragging {
+		delta := rl.GetMouseDelta()
+
+		w.Area.X = w.Area.X + delta.X
+		w.Area.Y = w.Area.Y + delta.Y
+	}
+}
+
+func (w *Window) Draw() {
+	rl.DrawRectangleRec(w.Area, rl.LightGray)
+	rl.DrawRectangleRec(w.barArea(), rl.DarkBlue)
+	contentArea := Area{
+		X:      w.Area.X + borderSize,
+		Y:      w.Area.Y + topBarSize,
+		Width:  w.Area.Width - 2*borderSize,
+		Height: w.Area.Height - borderSize - topBarSize,
+	}
+	w.Content.Layout(contentArea)
+	ctx := Context{}
+	w.Content.Draw(&ctx)
+}
