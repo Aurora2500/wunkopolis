ca02de94a666a29b13e67f6e10ef12991d5be712
Aurora <programming.aurora@gmail.com>
2024-08-16 22:28:29 +0100

    restructured for ease of debugging and profiling

--- main.go
+++ main.go
@@ -1,54 +1,5 @@
 package main
 
-import (
-	"wunkopolis/assets"
-	"wunkopolis/ui"
-
-	rl "github.com/gen2brain/raylib-go/raylib"
-)
-
-var backgroundColor = rl.Color{R: 0, G: 130, B: 120, A: 255}
-var Font rl.Font
-
 func main() {
-	rl.InitWindow(0, 0, "Wunkopolis")
-	defer rl.CloseWindow()
-	defer assets.Manager.Unload()
-
-	rl.SetTargetFPS(60)
-	rl.ToggleFullscreen()
-	Font = assets.Manager.GetFont("W95FA")
-	bottomBar := ui.Bar{}
-
-	w1 := ui.Window{
-		Area: ui.Area{
-			X:      0,
-			Y:      0,
-			Width:  632,
-			Height: 350,
-		},
-		Content: &ui.Flexbox{
-			Dir: ui.Col,
-			Elements: []ui.UIElem{
-				&ui.Flexbox{
-					Padding:  4,
-					Elements: []ui.UIElem{&ui.Button{Type: "Long"}, &ui.Button{Type: "Long"}, &ui.Button{Type: "Long"}},
-					Anchor:   ui.Center},
-			}},
-		Icon:  assets.Manager.GetTexture("Statistics"),
-		Title: "Statistics",
-	}
-	bottomBar.Setup()
-	w1.Setup(&bottomBar)
-	for !rl.WindowShouldClose() {
-		w1.Update()
-		rl.BeginDrawing()
-
-		rl.ClearBackground(backgroundColor)
-		bottomBar.Draw()
-		bottomBar.Update()
-		w1.Draw(Font)
-
-		rl.EndDrawing()
-	}
+	run_game()
 }
